6607e80403f4863fcc488a09eb709e0dbe9ed37c
25lp5076 User <25lp5076@administrators-iMac.local>
2026-01-09 12:26:40 +0800

    beginning

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  return (
+    <div className="">
+      <div className="bg-green-100 w-100vw h-165"></div>
+      <div className="bg-white w-100vw h-200"></div>
+    </div>
+  );
+}
